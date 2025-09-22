5227893b8d4f9129d8bc76aeca917748fb0832ad
prathoseraaj <prathoseraaj0312@gmail.com>
2025-09-23 01:06:43 +0530

    feat: setmode, setup and output

--- motor_system.py
+++ motor_system.py
@@ -5,3 +5,20 @@ try:
     import RPi.GPIO as GPIO
     print("STATUS: Running on REAL Raspberry Pi Hardware.")
 except:
+     print("WARNING: RPi.GPIO not found. Using MOCK mode for simulation.")
+     
+     class MockGPIO:
+         
+        HIGH, LOW, OUT, BCM = 1, 0, 1, 1
+         
+        def setmode(self,mode):
+            print("MOCK: GPIO mode set")
+        def setup(self, pin, mode): 
+            print(f"MOCK: Pin {pin} set up as OUTPUT.")
+        def output(self,pin,mode):
+            state = "HIGH" if value == self.HIGH else "LOW"
+            print(f"MOCK: Pin {pin} set to {state}.")
+        
+        
+            
+        
